6b9f76b9938b35ef9755b9d21d6cf66b33e1f6ee
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2023-12-25 21:27:24 -0500

    Update firebaseConfig.js

--- Logan-Project/LSStream/firebaseConfig.js
+++ Logan-Project/LSStream/firebaseConfig.js
@@ -13,13 +13,7 @@ import { ReactNativeAsyncStorage } from "@react-native-async-storage/async-stora
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyArNTcutEl7_T8edaxiydAZ9OETLtgEwtU",
-	authDomain: "n322-jacogarw.firebaseapp.com",
-	projectId: "n322-jacogarw",
-	storageBucket: "n322-jacogarw.appspot.com",
-	messagingSenderId: "467906406613",
-	appId: "1:467906406613:web:697c523391f04d037d9a5c",
-	measurementId: "G-SEK40FD9L5",
+	// FIREBASECONFIG FILES
 };
 
 // Initialize Firebase
